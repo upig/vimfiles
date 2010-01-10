0dc00915678d409ed7e277bd79de7e0f94b850fb
unknown <Administrator@.(none)>
2010-01-10 23:03:21 +0800

    数汉字的个数，看小说时经常用

---
+++ count.rb
@@ -0,0 +1,16 @@
+$KCODE='utf-8'
+require 'jcode'
+require'iconv'  
+class String  
+ def to_gbk  
+  Iconv.iconv("GBK//IGNORE","UTF-8//IGNORE",self).to_s  
+ end  
+ def to_utf8  
+  Iconv.iconv("UTF-8//IGNORE","GBK//IGNORE",self).to_s  
+ end  
+end  
+
+
+fileName =IO.read(ARGV[0]).chomp
+puts IO.read(fileName.to_gbk).jlength-1
+
